40ad4b5be85e79ae25fd78fae835602fc7471bd9
Vencill <wliu0028@student.monash.edu>
2019-09-01 22:00:57 +1000

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_W05.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
